27d31abe4234e314af17ff95f3519626cfe40ff0
Dana Lopez <dana.lopez@aol.com>
2021-05-19 08:48:14 -0400

    completed

--- Mod-03-Working-with-Numbers/Exercise-02/area_of_circle.js
+++ Mod-03-Working-with-Numbers/Exercise-02/area_of_circle.js
@@ -1,5 +1,6 @@
 "use strict";
 
 let radius = 6;
-let area = Math.PI * (radius ** 2);
+let area = Math.PI * (radius ** 2); // only works in the newest of browsers
+let area = Math.PI *  Math.pow(radius,2)
 console.log("The area of a circle with radius " + radius + " is " + area);
